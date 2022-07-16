0a698cae2898f54b48ca454882e3038faf3e827a
Matias Santos <yo@matiassantos.com>
2022-07-16 13:44:17 -0300

    config   svelt for static
    Signed-off-by: Matias Santos <yo@matiassantos.com>

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,26 @@
 import sveltePreprocess from 'svelte-preprocess'
+import adapter from "@sveltejs/adapter-static"; 
+// was "@sveltejs/adapter-auto"
 
-export default {
-  // Consult https://github.com/sveltejs/svelte-preprocess
-  // for more information about preprocessors
-  preprocess: sveltePreprocess()
-}
+const dev = "production" === "development";
+
+/** @type {import(""@sveltejs/kit").Config} */
+const config = {
+    kit: {
+        adapter: adapter({
+            pages: "docs",
+            assets: "docs"
+        }),
+        paths: {
+            // change below to your repo name
+            base: dev ? "" : "/vitejs-svelte",
+        },
+        // hydrate the <div id="svelte"> element in src/app.html
+        target: "#app"
+    },
+    // Consult https://github.com/sveltejs/svelte-preprocess
+    // for more information about preprocessors
+    preprocess: sveltePreprocess()
+};
+
+export default config;
