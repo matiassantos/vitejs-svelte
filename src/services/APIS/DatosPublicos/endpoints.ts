825eedaef8f1896a59c72985a6940399d64a69e2
Matias Santos <yo@matiassantos.com>
2022-07-18 12:24:16 -0300

    sample endpoints
    Signed-off-by: Matias Santos <yo@matiassantos.com>

---
+++ src/services/APIS/DatosPublicos/endpoints.ts
@@ -0,0 +1,6 @@
+export enum APIDatosPublicsEndpoints {
+    TIPO_CAMBIO = 'https://apis.datos.gob.ar/series/api/series/?ids=168.1_T_CAMBIOR_D_0_0_26&start_date=2018-07&limit=5000',
+    TASA_MORT_INF = 'https://apis.datos.gob.ar/series/api/series/?ids=tmi_arg&format=json',
+    TASA_PERS_FEME_FFAA = 'https://apis.datos.gob.ar/series/api/series/?ids=defensa_FAA_0006&limit=5000',
+    INDICE_PRECIOS_CONSUMIDOR = 'https://apis.datos.gob.ar/series/api/series/?ids=101.1_I2NG_2016_M_22:percent_change_a_year_ago&format=json'
+}
